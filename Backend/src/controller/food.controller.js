27f3ec08c03f6cef22c2c038b89a22e6f3f94760
Neha Sen <neha712513@gmail.com>
2022-06-10 12:27:40 +0530

    ok

--- Backend/src/controller/food.controller.js
+++ Backend/src/controller/food.controller.js
@@ -6,85 +6,45 @@ const router = express.Router();
 
 
 
-router.get("", async(req, res) => {
-    try{
-        const page = req.query.page || 1;
-        const pagesize = req.query.pagesize || 5;
-
-        const skip = (page - 1) * pagesize;
-
-        const products = await Product.find()
-        .skip(skip)
-        .limit(pagesize)
-        .lean()
-        .exec();
-
-        const totalPages = Math.ceil(
-            (await Product.find().countDocuments())
-        );
-
-
-        return res.status(200).send(products);
-
-    }
-    catch(err) {
-        return res.status(400).send(err);
-    }
-})
-
-// to sort the product by fabric 
-router.get("/sort/fabric", async (req, res) => {
-    const fabric = req.query.fabric;
-    const product = await Product
-        .find({ gender: { $eq: `${fabric}` } })
-        .lean()
-        .exec();
-    return res.status(201).send(product);
-})
-
-//to sort the product by colors
-router.get("/sort/color", async (req, res) => {
-    const color = req.query.color;
-    const prodColor = await Product
-        .find({ color: { $eq: `${color}` } })
-        .lean()
-        .exec();
-    return res.status(201).send(prodColor);
-})
-
-// //to sort the product by brands
-router.get("/sort/brand", async (req, res) => {
+router.get("/", async(req, res) => {
     try {
-        const brand = req.query.brand;
-    const prodBrand = await Product
-        .find({ brand: { $eq: `${brand}` } })
-        .lean()
-        .exec();
-    return res.status(200).send(prodBrand);
-    } catch (error) {
-        return res.status(500).send({message: error.message});
+        let page = req.query.page || 1
+        let pagesize = req.query.pagesize || 5
+        let brand = req.query.brand
+        let tag = req.query.tag
+        let price = req.query.price
+        
+        const skip = (page-1) * pagesize;
+
+        if(brand !== "all" && tag !== "all"){
+
+            const product=await Product.find({$and: [{brand:{$eq:brand}}, {tag: {$eq: tag}}]}).skip(skip).limit(pagesize).sort({price:price}).lean().exec()
+            const total_pages=Math.ceil((await Product.find({brand:{$eq:brand}}).countDocuments())/pagesize)
+            return res.send({product, total_pages})
+        }
+        else if(tag !== "all" && brand == "all"){
+            const product=await Product.find({tag: {$eq: tag}}).skip(skip).limit(pagesize).sort({price:price}).lean().exec()
+            const total_pages=Math.ceil((await Product.find({tag:{$eq:tag}}).countDocuments())/pagesize)
+            return res.send({product, total_pages})
+        }
+        else if(tag == "all" && brand !== "all"){
+            const product=await Product.find({brand: {$eq: brand}}).skip(skip).limit(pagesize).sort({price:price}).lean().exec()
+            const total_pages=Math.ceil((await Product.find({brand:{$eq:brand}}).countDocuments())/pagesize)
+            return res.send({product, total_pages})
+        }
+        else{
+            const product = await Product.find().skip(skip).limit(pagesize).lean().exec()
+            const total_pages = Math.ceil((await Product.find().sort({price:price}).countDocuments())/pagesize)
+          
+            return res.send({product, total_pages})
+        }
+    } 
+    catch (error) {
+        res.send(error)
     }
-});
-
-//to sort the product by types
-router.get("/sort/type", async (req, res) => {
-    const type = req.query.type;
-    const prodBrand = await Combi
-        .find({ type: { $eq: `${type}` } })
-        .lean()
-        .exec();
-    return res.status(201).send(prodBrand);
 })
 
-//to sort the product by price
-router.get("/sort/price", async (req, res) => {
-    const from = req.query.from;
-    const to = req.query.to;
-    const prodPrice = await Combi
-        .find({ $and: [{ price: { $gt: `${from}` } }, { price: { $lt: `${to}` } }] })
-        .lean()
-        .exec();
-    return res.status(201).send(prodPrice);
-})
+
+
 
 module.exports = router;
